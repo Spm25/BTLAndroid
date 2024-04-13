ef7da340392cb2300ce86a6f5e3c53c54bcb99a4
Hoang Duong <duongbn0000@gmail.com>
2024-04-13 23:20:30 +0700

    Thêm listview, nút huỷ.

--- app/src/main/java/com/example/btlandroid/InvoiceDetailsActivity.java
+++ app/src/main/java/com/example/btlandroid/InvoiceDetailsActivity.java
@@ -1,6 +1,7 @@
 package com.example.btlandroid;
-
+import android.app.Activity;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,58 +12,64 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import com.journeyapps.barcodescanner.ScanContract;
 import com.journeyapps.barcodescanner.ScanOptions;
-
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-
 public class InvoiceDetailsActivity extends AppCompatActivity {
     TextView txtTime;
     ImageButton btnScan;
+    ImageButton lvqr;
+    Button btnhuy;
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.invoice_details_layout);
         btnScan = findViewById(R.id.btnScan);
-        btnScan.setOnClickListener(v->
-        {
-            scanCode();
-        });
-
-        txtTime=(TextView) findViewById(R.id.tvdate);
+        txtTime = findViewById(R.id.tvdate);
+        btnhuy  = findViewById(R.id.btnHuy);
         Calendar calendar = Calendar.getInstance();
-
-        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy  ");
-        txtTime.append(dateFormat.format(calendar.getTime()));
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy ");
         SimpleDateFormat hourFormat = new SimpleDateFormat("hh:mm:ss a");
+        txtTime.setText(dateFormat.format(calendar.getTime()));
         txtTime.append(hourFormat.format(calendar.getTime()));
-        Button btnCancel = findViewById(R.id.btnHuy);
-        btnCancel.setOnClickListener(new View.OnClickListener() {
+        View itemView = getLayoutInflater().inflate(R.layout.lvitem_product_layout, null);
+        lvqr = itemView.findViewById(R.id.lvqr);
+        btnScan.setOnClickListener(v -> scanCode());
+        lvqr.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Kết thúc hoạt động hiện tại và quay lại hoạt động trước đó
-                finish();
+// Xử lý sự kiện khi nhấn vào ImageButton lvqr
+                Intent intent = new Intent(InvoiceDetailsActivity.this, MainActivity2.class);
+                startActivity(intent);
             }
         });
-        
-
+        btnhuy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+// Xử lý sự kiện khi nhấn vào ImageButton lvqr
+                Intent intent = new Intent(InvoiceDetailsActivity.this, InvoiceActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+    // Phương thức để quét mã
     private void scanCode() {
         ScanOptions options = new ScanOptions();
-        options.setPrompt("Volumn on to flash on");
+        options.setPrompt("Volume on to flash on");
         options.setBeepEnabled(true);
         options.setOrientationLocked(true);
         options.setCaptureActivity(CaptureAct.class);
-        barLaucher.launch(options);
+        barLauncher.launch(options);
     }
-    ActivityResultLauncher<ScanOptions> barLaucher = registerForActivityResult(new ScanContract(), result->{
-        if(result.getContents()!=null) {
+    // Kết quả của quét mã
+    ActivityResultLauncher<ScanOptions> barLauncher = registerForActivityResult(new ScanContract(), result -> {
+        if (result.getContents() != null) {
             AlertDialog.Builder builder = new AlertDialog.Builder(InvoiceDetailsActivity.this);
             builder.setTitle("Result");
             builder.setMessage(result.getContents());
             builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialogInterface, int i) {
-                    dialogInterface.dismiss();;
+                    dialogInterface.dismiss();
                 }
             }).show();
         }
